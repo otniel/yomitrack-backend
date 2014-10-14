e06e1cefdee3b886e902ce612d035ac3fa022653
Otniel Aguilar <otnieel.aguilar@gmail.com>
2014-10-13 23:03:02 -0500

    Se cambia en /feed la foto, por el array de todas las fotos

--- app/YomiTrack/Transformers/PromotionTransformer.php
+++ app/YomiTrack/Transformers/PromotionTransformer.php
@@ -22,9 +22,16 @@ class PromotionTransformer extends Transformer {
 
     public function transformFeed($promotion) {
         return [
+            'restaurant_id'    => (int) $promotion->restaurant_id,
             'name'             => $promotion->name,
             'restaurant_rate'  => (float) $promotion->restaurant_rate,
-            'restaurant_photo' => $promotion->photo1
+            'photos' => [
+                $promotion->photo1,
+                $promotion->photo2,
+                $promotion->photo3,
+                $promotion->photo4,
+                $promotion->photo5
+            ]
         ];
     }
 
--- app/models/Promotion.php
+++ app/models/Promotion.php
@@ -18,7 +18,14 @@ class Promotion extends Ardent {
         return DB::table('promotions')
             ->join('restaurant', 'restaurant.id', '=', 'promotions.restaurant_id')
             ->orderBy('promotions.updated_at', 'desc')
-            ->select('promotions.name', 'restaurant.photo1', 'restaurant.rate as restaurant_rate')
+            ->select('promotions.name',
+                     'restaurant.photo1',
+                     'restaurant.photo2',
+                     'restaurant.photo3',
+                     'restaurant.photo4',
+                     'restaurant.photo5',
+                     'restaurant.rate as restaurant_rate',
+                     'promotions.restaurant_id')
             ->paginate($limit);
     }
 }
