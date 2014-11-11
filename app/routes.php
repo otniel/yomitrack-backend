9884e17a81e01a2c3ed8251e3f5f8faa7bbe4232
Otniel Aguilar <otnieel.aguilar@gmail.com>
2014-11-11 16:34:35 -0600

    Redirect login

--- app/routes.php
+++ app/routes.php
@@ -15,6 +15,9 @@
     return array($sql);
 });*/
 
+Route::get('/', function() {
+    return Redirect::to('login');
+});
 // Sesiones
 Route::get('login', 'SessionsController@create', ['only' => ['create', 'store', 'destroy']]);
 Route::get('logout', ['as' => 'logout', 'uses' => 'SessionsController@destroy']);
